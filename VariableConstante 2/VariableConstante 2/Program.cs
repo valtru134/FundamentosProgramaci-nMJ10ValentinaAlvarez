a7fb8f17d41c5f605bdae4298ebe679dd0f963a0
B09S104est <B09S104est@MEDAULAS.UPB>
2026-07-28 11:19:03 -0500

    Este es mi primer commit

---
+++ VariableConstante 2/VariableConstante 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariableConstante_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el codigo fuente
+            string nombre="Valentina";
+            Console.WriteLine("Bienvenida"+ nombre +"al curso de fudamentos de programación");
+        }
+    }
+}
