0287853f1263f89925e1e6c8c833bf03f46d6f3e
valtru134 <valentina.alvarezt@upb.edu.co>
2026-08-11 11:27:40 -0500

    5.CondicionalesAnidados

---
+++ 5CondicionalesAnidados/5CondicionalesAnidados/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace _5CondicionalesAnidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales anidados
+            //b.    Diseñar un algoritmo que lea tres números A, B, C y visualice en pantalla el valor más grande. El usuario debe ingresar tres valores deferentes
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            Console.WriteLine("Se deben iggresar tres valores diferente");
+            Console.WriteLine("Ingrese el valor del número 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 3");
+            num3 = int.Parse(Console.ReadLine());
+
+            if(num1>num2)
+            {
+                //verdadera
+                if (num1>num3)
+                {
+                    Console.WriteLine("El número 1 es el mayor");
+                }
+                else
+                {
+
+                    Console.WriteLine("El número 1 es el mayor");
+                }
+            }
+            else
+            {
+                //falsa
+                if(num2>num3)
+                {
+                    Console.WriteLine("El número 2 es el mayor");
+                }
+
+                else
+                {
+                    Console.WriteLine("El número 3 es el mayor");
+
+                }
+            }
+        }
+    }
+}
