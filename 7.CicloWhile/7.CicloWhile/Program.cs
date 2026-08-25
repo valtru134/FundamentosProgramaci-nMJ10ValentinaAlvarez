2499c762ca45b3a0a6b8d2d9da22565b0f08e5f5
valtru134 <valentina.alvarezt@upb.edu.co>
2026-08-25 11:27:25 -0500

    7.CiclosWhile

---
+++ 7.CicloWhile/7.CicloWhile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace _7.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1,  Escribe un algoritmo que permita generar los primeros 5 números enteros positivos y realizar y mostrar suma.
+           // int contador = 1;
+            //int acumulador = 0;
+
+           // while (contador <=5)
+            {
+               // acumulador = acumulador + contador;
+                //contador++;
+            }
+
+           //Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+
+            // Crear un algoritmo que permita calcular la factorial de un número dado por el usuario.(5! = 1*2*3*4*%
+
+            int contador = 1;
+            int acumulador = 0;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un número para factorisar");
+            numero =int.Parse (Console.ReadLine());
+
+            while(numero == contador)
+            {
+
+            }
+
+
+
+        }
+    }
+}
