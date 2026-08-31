65197ac18ee0f22ede255be9d1a14389b82edfb7
valtru134 <valentina.alvarezt@upb.edu.co>
2026-08-30 20:37:10 -0500

    10. CiclosWhile3

---
+++ 10. CiclosWhile/10. CiclosWhile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace _10.CiclosWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escribe un algoritmo en el que el computador elija un número aleatorio entre 1 y 100. El usuario debe adivinar el número, y el programa debe decirle si su intento es demasiado alto, demasiado bajo o correcto. Usa un ciclo while para seguir pidiendo intentos hasta que el usuario adivine el número. Utilizar la función Random para elegir el número aleatorio.
+            Random random = new Random();
+            int numeroSecreto = random.Next(1, 101);
+            int intentos = 0;
+            int contador = 0;
+
+            Console.WriteLine("Adivine el número entre 1 y 100");
+
+            while (intentos != numeroSecreto )
+            {
+                Console.WriteLine("Escribe el número que crees:");
+                intentos = int.Parse(Console.ReadLine());
+                contador = contador + 1;
+
+                if (intentos < numeroSecreto)
+                {
+                    Console.WriteLine("Demasiado bajo, intenta de nuevo");
+                }
+                else if(intentos > numeroSecreto)
+                {
+                    Console.WriteLine("Demasiado alto, intentelo de nuevo");
+
+                }
+                else
+                {
+                    Console.WriteLine("Felicidades, ha adivinado el número");
+                }
+            }
+        }
+    }
+}
---
+++ 10. CiclosWhile/10. CiclosWhile/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("10. CiclosWhile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("10. CiclosWhile")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f9549d6c-2501-4faf-bc0b-a6117b9a54e4")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
